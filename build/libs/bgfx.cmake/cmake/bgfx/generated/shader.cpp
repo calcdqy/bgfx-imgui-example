2f498201af7d03e03f6649e831889113b498fd53
calcdqy <104544646+calcdqy@users.noreply.github.com>
2025-03-26 00:59:12 +0800

    delete build and vscode setting

--- build/libs/bgfx.cmake/cmake/bgfx/generated/shader.cpp
+++
@@ -1,3 +0,0 @@
-#include "C:/Users/CALCDQY/Documents/sdl3-bgfx-imgui example/libs/bgfx.cmake/bgfx/src/shader.cpp"
-#include "C:/Users/CALCDQY/Documents/sdl3-bgfx-imgui example/libs/bgfx.cmake/bgfx/src/shader_dxbc.cpp"
-#include "C:/Users/CALCDQY/Documents/sdl3-bgfx-imgui example/libs/bgfx.cmake/bgfx/src/shader_spirv.cpp"
--- build/libs/bgfx.cmake/cmake/bgfx/generated/vertexlayout.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/CALCDQY/Documents/sdl3-bgfx-imgui example/libs/bgfx.cmake/bgfx/src/vertexlayout.cpp"
